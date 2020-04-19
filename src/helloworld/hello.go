0425acec49182036fde375ca82f32450baba0292
Thanapon Yenjam <thanapon_team@hotmail.com>
2020-04-19 23:43:55 +0700

    GO GET

--- src/helloworld/hello.go
+++ src/helloworld/hello.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"myFmt"
+	"log"
+	"github.com/golang/example/stringutil"
 )
 
 func main() {
 	fmt.Println("Hello World")
 	myFmt.Println("Test")
+	log.Println("Test Log")
+	stringutil.Reverse("Helloworld")
 }
