c622db0d0670f8be004d5ce18016f0fbd3d87268
Thanapon Yenjam <thanapon_team@hotmail.com>
2020-04-20 23:15:08 +0700

    Pointer

--- src/sandbox/sandbox.go
+++ src/sandbox/sandbox.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	a := float64(2)
-	b := math.Sqrt(a)
-	c := 1 + b
-	fmt.Println("C = ",c)
+	
+	x := 70
+	var p *int	 // ประกาศตัวแปร Pointer (*) ชนิด int มีค่า default = nil
+	p = &x		 // กำหนดให้ value ของ p = address ของ x
+
+	fmt.Println("Value x : ", x)
+	fmt.Println("Address &x : ", &x)		// &x จะเป็นการอ้างอิงถึง Address ของตัวแปร x
+	fmt.Println("dereference Value *(&x)  : ", *(&x)) // *(&x) จะเป็นการ dereference ค่าของตัวแปร x (x ไม่ใช่ตัวแปร pointer)
+	fmt.Println("---------------------")
+	fmt.Println("Value p : ", p)
+	fmt.Println("Address &p : ", &p)
+	fmt.Println("dereference Value p  : ", *p)	// *p จะเป็นการ dereference ค่าของตัวแปร x ที่ตัวแปร p เก็บไว้
 }
