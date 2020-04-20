52ea2efd6590ad3eaf9ab62c745855ce8ca77cff
Thanapon Yenjam <thanapon_team@hotmail.com>
2020-04-20 22:09:41 +0700

    Variables Declaration

--- src/helloworld/hello.go
+++ src/helloworld/hello.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
+
+	"github.com/golang/example/stringutil"
 )
+
+// import (
+// 	"fmt"
+// 	"log"
+
+// 	"github.com/golang/example/stringutil"
+// )
 
 func main() {
 	log.Println("Vendor Directory")
+	fmt.Println(stringutil.Reverse("Test"))
 }
