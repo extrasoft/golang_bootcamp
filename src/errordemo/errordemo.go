d13adf69b1ac43517293f6954c6fb2afdfcc6f27
Thanapon Yenjam <thanapon_team@hotmail.com>
2020-05-01 22:08:52 +0700

    error retry

--- src/errordemo/errordemo.go
+++ src/errordemo/errordemo.go
@@ -4,19 +4,54 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"time"
 )
 
-const dbReady = true
+const dbReady = false
 const balance = 200
+const numberToSuccess = 100000 // จำนวนที่จำลอง connect สำเร็จ
+
+func connectDB(nTry int) error {
+	if nTry == numberToSuccess {
+		return nil
+	}
+
+	return errors.New("busy")
+}
+
+func waitForDatabase() error {
+	timeout := 3 * time.Second
+	deadline := time.Now().Add(timeout) // กำหนดเวลาสิ้นสุดในการ connect เป็น 3 วินาที
+
+	// วนลูปเพื่อกำหนดเวลาในการลอง connect ก่อนหมดเวลา
+	// เช่น มีเวลาในการลอง connect กี่ครั้งก็ได้ภายใน 3 วิ
+	for tries := 0; time.Now().Before(deadline); tries++ {
+		err := connectDB(tries)
+		if err == nil {
+			return nil
+		}
+		log.Printf("%v database is not responding (%v); retrying...",tries, err)
+		
+		// ถ้าต้องการให้การ trying แต่ละครั้งหน่วงเวลา 1 วินาที
+		// time.Sleep(time.Second)
+		// หรือ
+		// time.Sleep(time.Second << uint(tries))	// 1 2 4 8
+	}
+
+	// ถ้าหมดเวลาแล้วยัง connect ไม่ได้ก็จะปริ้น log แล้วออกจากการทำงาน
+	return fmt.Errorf("waitForDatabase: timeout %v", timeout)
+}
 
 func getBalance() (int, error) {
 	if !dbReady {
-		return 0, errors.New("withdraw: database is down")
+		// log.Println("retrying....")
+		if err := waitForDatabase(); err != nil {
+			return 0, fmt.Errorf("getBalance(), %v", err)
+		}
+		// return 0, errors.New("withdraw: database is down")
 	}
 	return balance, nil
 }
-
-
 
 func withdraw(amount int) (int, error) {
 	balance, err := getBalance()
@@ -32,10 +67,10 @@ func withdraw(amount int) (int, error) {
 }
 
 func main() {
-	log.SetFlags(0)	// 0 คือไม่มี pattern log เลย
+	log.SetFlags(0) // 0 คือไม่มี pattern log เลย
 	amount, err := withdraw(200)
 	if err != nil {
-		
+
 		// fmt.Println("Main(): ", err)
 		// os.Exit(1)	//	os.Exit(1) หมายถึงออกจากโปรแกรม
 
